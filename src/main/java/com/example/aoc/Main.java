47baf39fd368ae712de79dd7970ad2c2d7e59f68
maria <mariacodes77@gmail.com>
2025-12-03 22:49:49 +0300

    chore: initialize project structure

---
+++ src/main/java/com/example/aoc/Main.java
@@ -0,0 +1,18 @@
+package com.example.aoc;
+
+import com.example.aoc.day01.Day01;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Usage: java -jar ... <day>");
+            return;
+        }
+        switch (args[0]) {
+            case "1": Day01.run(); break;
+            //case "2": Day02.run(); break;
+            // ...
+            default: System.out.println("Unknown day: " + args[0]);
+        }
+    }
+}
---
+++ src/main/java/com/example/aoc/day01/Day01.java
@@ -0,0 +1,5 @@
+package com.example.aoc.day01;
+
+public class Day01 {
+    public static void run() {}
+}
