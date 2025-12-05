e95cf51f41dec9fdf2f832098e8480208bf6cc7d
maria <mariacodes77@gmail.com>
2025-12-05 22:17:08 +0300

    feat(day01): add solution for day 01 part 2

--- src/main/java/com/example/aoc/day01/Day01.java
+++ src/main/java/com/example/aoc/day01/Day01.java
@@ -9,6 +9,7 @@ public class Day01 {
     public static void run() throws IOException {
         List<String> list = Files.readAllLines(Paths.get("src/main/resources/input.txt"));
         System.out.println("Result: " + solve(list));
+        System.out.println("Result: " + partTwo(list));
     }
 
     public static int solve(List<String> list) {
@@ -29,7 +30,35 @@ public class Day01 {
                 count++;
             }
         }
+        return count;
+    }
 
+    public static int partTwo(List<String> list) {
+        int position = 50;
+        int count = 0;
+
+        for (String s : list) {
+            char f = s.charAt(0);
+            int d = Integer.parseInt(s.substring(1));
+
+            int firstHit;
+            if (f == 'L') {
+                firstHit = (position == 0) ? 100 : position;
+            } else {
+                int x = (100 - position) % 100;
+                firstHit = (x == 0) ? 100 : x;
+            }
+
+            if (d >= firstHit) {
+                count += 1 + (d - firstHit) / 100;
+            }
+
+            if (f == 'L') {
+                position = Math.floorMod(position - d, 100);
+            } else {
+                position = Math.floorMod(position + d, 100);
+            }
+        }
         return count;
     }
 }
