7da9b564b6101806589a1c0d9c47117c5503b325
maria <mariacodes77@gmail.com>
2025-12-06 20:45:25 +0300

    feat(day02): add solution for day 02 part 1

--- src/main/java/com/example/aoc/day02/Day02.java
+++ src/main/java/com/example/aoc/day02/Day02.java
@@ -26,7 +26,23 @@ public class Day02 {
 	}
 
 	private static String partOne(List<String> lines) {
-		return String.valueOf(lines.size());
+		long sum = 0;
+		String[] split = lines.get(0).split(",");
+        for (String string : split) {
+            long startingElement = Long.parseLong(string.split("-")[0]);
+            long endingElement = Long.parseLong(string.split("-")[1]);
+
+            for (long j = startingElement; j <= endingElement; j++) {
+                String s = String.valueOf(j);
+                int middleIndex1 = s.length() / 2;
+                String firstHalf1 = s.substring(0, middleIndex1);
+                String secondHalf1 = s.substring(middleIndex1);
+                if (firstHalf1.equals(secondHalf1)) {
+                    sum += j;
+                }
+            }
+        }
+		return String.valueOf(sum);
 	}
 
 	private static String partTwo(List<String> lines) {
