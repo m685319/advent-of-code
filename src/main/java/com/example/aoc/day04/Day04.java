bc2382876fd858f0b3f262eaa46c23ee2838241f
Abhishek Verma <41110939+abhishekvermaa10@users.noreply.github.com>
2025-12-31 23:22:06 +0530

    Added test input from day 7 to 12 (#2)
    * Added test input from day 7 to 12
    
    * update

--- src/main/java/com/example/aoc/day04/Day04.java
+++ src/main/java/com/example/aoc/day04/Day04.java
@@ -26,7 +26,66 @@ public class Day04 {
 	}
 
 	private static String partOne(List<String> lines) {
-		return String.valueOf(lines.size());
+		int rows = lines.size();
+		int cols = lines.get(0).length();
+		char[][] grid = new char[rows][cols];
+		for (String line : lines) {
+			grid[lines.indexOf(line)] = line.toCharArray();
+		}
+		int count = countAccessble(grid, rows, cols);
+		return String.valueOf(count);
+	}
+
+	private static int countAccessble(char[][] grid, int rows, int cols) {
+		int accessibleCount = 0;
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				char current = grid[i][j];
+				if (current != '@') {
+					continue;
+				}
+				boolean canAccess = isAccessible(grid, i, j, rows, cols);
+				if (canAccess) {
+					accessibleCount++;
+				}
+			}
+		}
+		return accessibleCount;
+	}
+
+	private static boolean isAccessible(char[][] grid, int i, int j, int rows, int cols) {
+		int count = 0;
+		if (j + 1 < cols) {
+			count += grid[i][j + 1] == '@' ? 1 : 0;
+		}
+		if (j - 1 >= 0) {
+			count += grid[i][j - 1] == '@' ? 1 : 0;
+		}
+
+		if (i - 1 >= 0) {
+			count += grid[i - 1][j] == '@' ? 1 : 0;
+			if (j + 1 < cols) {
+				count += grid[i - 1][j + 1] == '@' ? 1 : 0;
+			}
+			if (j - 1 >= 0) {
+				count += grid[i - 1][j - 1] == '@' ? 1 : 0;
+			}
+		}
+
+		if (i + 1 < rows) {
+			count += grid[i + 1][j] == '@' ? 1 : 0;
+			if (j + 1 < cols) {
+				count += grid[i + 1][j + 1] == '@' ? 1 : 0;
+			}
+			if (j - 1 >= 0) {
+				count += grid[i + 1][j - 1] == '@' ? 1 : 0;
+			}
+		}
+
+		if (count < 4) {
+			return true;
+		}
+		return false;
 	}
 
 	private static String partTwo(List<String> lines) {
